401f603847eabe2c50c5bb08ed94dbfa326b53d7
vishvAsaH <vishvas.vasuki@gmail.com>
2017-08-26 08:43:36 -0700

    Include data

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
   # installed, specify them here.  If using Python 2.6 or less, then these
   # have to be included in MANIFEST.in as well.
   package_data={
-      'jyotisha': ['namesmd', "panchangam/data/*", "panchangam/data/precomputed/*"],
+      'jyotisha': ['names/*.md', "panchangam/data/*", "panchangam/data/precomputed/*"],
   },
 
   # Although 'package_data' is the preferred approach, in some case you may
