b8c0d6ba5a7c3a9f2aa322a5e7d51e6e770441d3
vishvAsaH <vishvas.vasuki@gmail.com>
2017-08-26 22:37:40 -0700

    Bump up version, add pandas dependency.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
   # Versions should comply with PEP440.  For a discussion on single-sourcing
   # the version across setup.py and the project code, see
   # https://packaging.python.org/en/latest/single_source_version.html
-  version='0.0.6',
+  version='0.0.7',
 
   description='Tools for computations involved in the jyotiSha vedAnga',
   long_description=long_description,
@@ -82,7 +82,7 @@ setup(
   # https://packaging.python.org/en/latest/requirements.html
   install_requires=[
     'pyswisseph',
-    'scipy',
+    'scipy', 'pandas',
     'indic_transliteration',
     'icalendar',
     'pytz',
