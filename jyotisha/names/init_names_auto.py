26a08ed381974c04da9cb47754f2235369e83f0c
Karthik Raman <karthik.raman@gmail.com>
2017-12-17 17:31:06 +0530

    additional scripts

--- jyotisha/names/init_names_auto.py
+++ jyotisha/names/init_names_auto.py
@@ -5,6 +5,7 @@ import ast
 
 import os
 import logging
+from indic_transliteration import xsanscript as sanscript
 
 logging.basicConfig(
   level=logging.DEBUG,
@@ -13,6 +14,7 @@ logging.basicConfig(
 
 CODE_ROOT = os.path.dirname(os.path.dirname(__file__))
 
+scripts = [sanscript.DEVANAGARI, sanscript.IAST]
 
 def init_names_auto(fname=os.path.join(CODE_ROOT, 'names/data/translation_table_HK.json')):
   """Read various nakShatra, samvatsara, mAsa and such names from a file return a dict with all of that.
@@ -22,5 +24,9 @@ def init_names_auto(fname=os.path.join(CODE_ROOT, 'names/data/translation_table_
   with open(fname) as f:
     import json
     names_dict = json.load(f)
-    logging.debug(json.dumps(names_dict, sort_keys=True, indent=2))
+    # logging.debug(json.dumps(names_dict, sort_keys=True, indent=2))
+    for dictionary in names_dict:
+      for scr in scripts:
+        names_dict[dictionary][scr] = [sanscript.transliterate(name, 'hk', scr) for name in names_dict[dictionary]['hk']]
+    # logging.debug(json.dumps(names_dict, sort_keys=True, indent=2))
     return names_dict
