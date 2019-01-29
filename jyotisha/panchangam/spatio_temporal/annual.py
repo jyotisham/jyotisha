df99098ae4f39328ab63a9436b03702a2e08a1d8
vvasuki <vishvas.vasuki@gmail.com>
2019-01-29 14:37:02 -0800

    format

--- jyotisha/panchangam/spatio_temporal/annual.py
+++ jyotisha/panchangam/spatio_temporal/annual.py
@@ -201,28 +201,18 @@ class Panchangam(common.JsonObject):
             self.rashi_data[d] = daily_panchaangas[d].rashi_data
             if compute_lagnams:
                 self.lagna_data[d] = daily_panchaangas[d].get_lagna_data(self.jd_sunrise[d])
-
+                
     def assignLunarMonths(self):
         last_d_assigned = 0
-        last_new_moon_start, last_new_moon_end = temporal.get_angam_span(self.jd_start -
-                                                                         self.tithi_sunrise[1] - 2,
-                                                                         self.jd_start - self.tithi_sunrise[1] + 2,
-                                                                         temporal.TITHI, 30,
-                                                                         ayanamsha_id=self.ayanamsha_id)
-        this_new_moon_start, this_new_moon_end = temporal.get_angam_span(last_new_moon_start + 24,
-                                                                         last_new_moon_start + 32, temporal.TITHI,
-                                                                         30,
-                                                                         ayanamsha_id=self.ayanamsha_id)
+        last_new_moon_start, last_new_moon_end = temporal.get_angam_span(
+            self.jd_start - self.tithi_sunrise[1] - 2, self.jd_start - self.tithi_sunrise[1] + 2,   temporal.TITHI, 30, ayanamsha_id=self.ayanamsha_id)
+        this_new_moon_start, this_new_moon_end = temporal.get_angam_span(last_new_moon_start + 24, last_new_moon_start + 32, temporal.TITHI, 30, ayanamsha_id=self.ayanamsha_id)
         # Check if current mAsa is adhika here
         isAdhika = temporal.get_solar_rashi(last_new_moon_end, ayanamsha_id=self.ayanamsha_id) == \
                    temporal.get_solar_rashi(this_new_moon_end, ayanamsha_id=self.ayanamsha_id)
 
         while last_new_moon_start < self.jd_start + 367:
-            next_new_moon_start, next_new_moon_end = temporal.get_angam_span(this_new_moon_start + 24,
-                                                                             this_new_moon_start + 32,
-                                                                             temporal.TITHI,
-                                                                             30,
-                                                                             ayanamsha_id=self.ayanamsha_id)
+            next_new_moon_start, next_new_moon_end = temporal.get_angam_span(this_new_moon_start + 24, this_new_moon_start + 32, temporal.TITHI, 30, ayanamsha_id=self.ayanamsha_id)
             for i in range(last_d_assigned + 1, last_d_assigned + 32):
                 last_solar_month = temporal.get_solar_rashi(this_new_moon_end, ayanamsha_id=self.ayanamsha_id)
 
