5463959a5a3bd6d68c081e9d3ea81cd991d0c05d
Karthik Raman <karthik.raman@gmail.com>
2019-02-15 21:25:35 +0530

    Update test_daily_tex.py

--- tests/spatio_temporal/test_daily_tex.py
+++ tests/spatio_temporal/test_daily_tex.py
@@ -33,6 +33,19 @@ def test_panchanga_chennai_2019():
       assert orig_tex.read() == current_tex.read()
 
 
+def test_panchanga_orinda_2019():
+  panchangam_2019 = JsonObject.read_from_file(filename=os.path.join(TEST_DATA_PATH, 'Orinda-2019.json'))
+  panchangam_2019.update_festival_details()
+  orig_tex_file = os.path.join(TEST_DATA_PATH, 'daily-cal-2019-Orinda-deva.tex')
+  daily_template_file = open(os.path.join(CODE_ROOT, 'jyotisha/panchangam/data/templates/daily_cal_template.tex'))
+  current_tex_output = os.path.join(TEST_DATA_PATH, 'daily-cal-2019-Orinda-deva.tex.local')
+  writeDailyTeX(panchangam_2019, daily_template_file, compute_lagnams=False, output_stream=open(current_tex_output, 'w'))
+
+  with open(orig_tex_file) as orig_tex:
+    with open(current_tex_output) as current_tex:
+      assert orig_tex.read() == current_tex.read()
+
+
 def test_panchanga_chennai_2018():
   panchangam_2018 = JsonObject.read_from_file(filename=os.path.join(TEST_DATA_PATH, 'Chennai-2018.json'))
   panchangam_2018.update_festival_details()
@@ -49,3 +62,4 @@ def test_panchanga_chennai_2018():
 if __name__ == '__main__':
   test_panchanga_chennai_2018()
   test_panchanga_chennai_2019()
+  test_panchanga_orinda_2019()
