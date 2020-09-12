d7039689709d230fb46485457ec96b9ea291812a
vvasuki <vishvas.vasuki@gmail.com>
2020-09-12 10:55:10 +0530

    CU fixing https://github.com/sanskrit-coders/jyotisha/issues/46

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
+import os
+
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
@@ -98,12 +100,8 @@ setup(
   #     'test': ['coverage'],
   # },
 
-  # If there are data files included in your packages that need to be
-  # installed, specify them here.  If using Python 2.6 or less, then these
-  # have to be included in MANIFEST.in as well.
-  package_data={
-      'jyotisha': ['names/data/*', "panchangam/data/*", "panchangam/data/templates/*", "panchangam/data/templates/fonts/*"],
-  },
+
+  include_package_data = True,
 
   # Although 'package_data' is the preferred approach, in some case you may
   # need to place data files outside of your packages. See:
