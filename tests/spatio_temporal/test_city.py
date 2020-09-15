f966b2204635f3aa1691a598e2ca18be7861fcdc
vvasuki <vishvas.vasuki@gmail.com>
2020-09-15 12:28:37 +0530

    New API to get graha rises

--- tests/spatio_temporal/test_city.py
+++ tests/spatio_temporal/test_city.py
@@ -9,9 +9,9 @@ def test_get_timezone_offset_hours_from_date():
     assert offset == -8
 
 def test_get_timezone_offset_hours_from_date():
-    city = City.from_address_and_timezone('Cupertino, CA', "America/Los_Angeles")
-    offset = city.get_timezone_offset_hours_from_date(year=2018, month=11, day=11)
+    offset = Timezone("America/Los_Angeles").get_timezone_offset_hours_from_date(year=2018, month=11, day=11)
     assert offset == -8
+
 
 def test_get_local_time():
     city = City.from_address_and_timezone(address="Mountain View, CA", timezone_str="America/Los_Angeles")
@@ -29,3 +29,9 @@ def test_jd_from_local_time():
     jd = Timezone(city.timezone).local_time_to_julian_day(year=2018, month=11, day=11, hours=6, minutes=0, seconds=0)
     import numpy.testing
     numpy.testing.assert_approx_equal(actual=jd, desired=2458433.750000, significant=7)
+
+
+def test_moonrise_time():
+    city = City.from_address_and_timezone('Bengaluru', "Asia/Calcutta")
+    from jyotisha.panchangam.temporal import Graha
+    assert city.get_rising_time(julian_day_start=2459107.33, body=Graha.MOON) == 2459107.4296293524
