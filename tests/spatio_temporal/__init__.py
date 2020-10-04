9d8218ea177e37aadf6f4ce31484009b1deac0b5
vvasuki <vishvas.vasuki@gmail.com>
2020-10-04 10:35:07 +0530

    Retire compute_lagnas option

--- tests/spatio_temporal/__init__.py
+++ tests/spatio_temporal/__init__.py
@@ -1,4 +1,3 @@
 from jyotisha.panchaanga.spatio_temporal import City
-from panchaanga import spatio_temporal
 
-chennai = spatio_temporal.City.get_city_from_db("Chennai")
+chennai = City.get_city_from_db("Chennai")
