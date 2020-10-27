5abb86c575b4e951358938520ed14a116fc7a0d0
vvasuki <vishvas.vasuki@gmail.com>
2020-10-27 09:21:11 +0530

    Cleanup.

--- jyotisha_tests/temporal/festival/applier/solar_test.py
+++ jyotisha_tests/temporal/festival/applier/solar_test.py
@@ -15,7 +15,7 @@ def test_daily_solar_viSukkaNi():
   panchaanga_15 = daily.DailyPanchaanga(city=chennai, date=Date(year=2018, month=4, day=15), previous_day_panchaanga=panchaanga_14)
   DailySolarAssigner(day_panchaanga=panchaanga_15, previous_day_panchaangas=[panchaanga_14]).apply_month_day_events()
 
-  panchaanga_16 = daily.DailyPanchaanga(city=chennai, date=Date(year=2018, month=4, day=16), previous_day_panchaangas=[panchaanga_15])
+  panchaanga_16 = daily.DailyPanchaanga(city=chennai, date=Date(year=2018, month=4, day=16), previous_day_panchaanga=panchaanga_15)
   DailySolarAssigner(day_panchaanga=panchaanga_16, previous_day_panchaangas=[panchaanga_15]).apply_month_day_events()
  
   panchaanga_17 = daily.DailyPanchaanga(city=chennai, date=Date(year=2018, month=4, day=17), previous_day_panchaanga=panchaanga_16)
