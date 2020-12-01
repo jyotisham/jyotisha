183ad18ae4e70e546268ed28797457ce8cb20465
vvasuki <vishvas.vasuki@gmail.com>
2020-12-01 17:17:23 +0530

    Fix build

--- jyotisha/panchaanga/temporal/__main__.py
+++ jyotisha/panchaanga/temporal/__main__.py
@@ -1,8 +1,4 @@
-import logging
-
-from jyotisha.panchaanga import temporal
 from jyotisha.panchaanga.spatio_temporal import City
-from jyotisha.panchaanga.temporal.time import Timezone
 
 city = City.from_address_and_timezone(address="Mountain View, CA", timezone_str="America/Los_Angeles")
 # time = Timezone(city.timezone).local_time_to_julian_day(year=2019, month=1, day=21, hours=18, minutes=32, seconds=0)
