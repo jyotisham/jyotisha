7edbcb0e42d1884d7841ceb36b4a3e8a16ac635c
vvasuki <vishvas.vasuki@gmail.com>
2020-12-01 20:01:34 +0530

    Fix build, link to ics

--- jyotisha/panchaanga/writer/generation_project/__init__.py
+++ jyotisha/panchaanga/writer/generation_project/__init__.py
@@ -15,9 +15,9 @@ from jyotisha.panchaanga.writer import ics, md
 output_dir = os.path.join(os.path.dirname(os.path.dirname(jyotisha.__file__)), "hugo_source", "content", "output")
 
 
-def dump_ics_md_pair(panchaanga):
+def dump_ics_md_pair(panchaanga, period_str):
   ics_calendar = ics.compute_calendar(panchaanga)
-  output_file_ics = os.path.join(output_dir, panchaanga.city.name, str(panchaanga.computation_system), "shaka_year", '%d.ics' % (panchaanga.start_date.year))
+  output_file_ics = os.path.join(output_dir, panchaanga.city.name, str(panchaanga.computation_system), "shaka_year", '%s.ics' % period_str)
   ics.write_to_file(ics_calendar, output_file_ics)
 
   md_file = MdFile(file_path=output_file_ics.replace(".ics", ".md"), frontmatter_type=MdFile.YAML)
@@ -29,13 +29,13 @@ def dump_ics_md_pair(panchaanga):
 def dump_common(year, city):
   computation_system = ComputationSystem.MULTI_NEW_MOON_SIDEREAL_MONTH_ADHIKA__CHITRA_180
   panchaanga = annual.get_panchaanga_for_shaka_year(city=city, year=year, computation_system=computation_system)
-  dump_ics_md_pair(panchaanga=panchaanga)
+  dump_ics_md_pair(panchaanga=panchaanga, period_str=str(year))
 
 
 def dump_kauNDinyAyana(year, city):
   computation_system = ComputationSystem.SOLSTICE_POST_DARK_10_ADHIKA__CHITRA_180
   computation_system.options.fest_repos = [RulesRepo(name="gRhya/general")]
   tropical_panchaanga = annual.get_panchaanga_for_shaka_year(city=city, year=year, computation_system=computation_system, allow_precomputed=False)
-  dump_ics_md_pair(panchaanga=tropical_panchaanga)
+  dump_ics_md_pair(panchaanga=tropical_panchaanga, period_str=str(year))
   
 
