cfbb3ffc23da1a3f549bcd4296ba7ee75a52f2e0
vvasuki <vishvas.vasuki@gmail.com>
2020-12-09 20:46:45 +0530

    Bots

--- jyotisha/bot/telegram/__init__.py
+++ jyotisha/bot/telegram/__init__.py
@@ -8,5 +8,5 @@ def send_panchaanga(channel_id, token, md_url_base):
   bot = telegram.Bot(token=token)
   shaka_year = ""
   url = "%s" % (md_url_base)
-  md = urlopen(url).read()
-  bot.sendMessage(chat_id = channel_id, text = md)
+  md = urlopen(url).read().decode("utf-8")
+  bot.sendMessage(chat_id="-" + channel_id, text=md)
