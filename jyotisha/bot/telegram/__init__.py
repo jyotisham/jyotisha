4dd4da0cb4b157ad56b2724d661f1f43cf7bceca
vvasuki <vishvas.vasuki@gmail.com>
2020-12-10 08:24:38 +0530

    Fix bot

--- jyotisha/bot/telegram/__init__.py
+++ jyotisha/bot/telegram/__init__.py
@@ -10,7 +10,7 @@ def send_panchaanga(channel_id, token, md_url_base, html_url_base):
   bot = telegram.Bot(token=token)
   bengaLUru = spatio_temporal.City.get_city_from_db("sahakAra nagar, bengaLUru")
   today = bengaLUru.get_timezone_obj().current_time()
-  day_path = "%04d_monthly/%04d-%02d/%04d-%02d-%02d" % (today.year, today.year, today.month, today.year, today.month, today.day)
+  day_path = "gregorian/%04d_monthly/%04d-%02d/%04d-%02d-%02d" % (today.year, today.year, today.month, today.year, today.month, today.day)
   md_url = "%s/%s.md" % (md_url_base, day_path)
   html_url = "%s/%s/" % (html_url_base, day_path)
   md = "%s\n\n%s" % (html_url, urlopen(md_url).read().decode("utf-8"))
