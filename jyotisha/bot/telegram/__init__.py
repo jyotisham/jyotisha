b831417fc4a55cf6c01b13af930b9467f6b8f52d
vvasuki <vishvas.vasuki@gmail.com>
2020-12-11 07:54:55 +0530

    Better logs.

--- jyotisha/bot/telegram/__init__.py
+++ jyotisha/bot/telegram/__init__.py
@@ -1,4 +1,4 @@
-import argparse
+import logging
 from urllib.request import urlopen
 
 import telegram
@@ -13,5 +13,7 @@ def send_panchaanga(channel_id, token, md_url_base, html_url_base):
   day_path = "gregorian/%04d_monthly/%04d-%02d/%04d-%02d-%02d" % (today.year, today.year, today.month, today.year, today.month, today.day)
   md_url = "%s/%s.md" % (md_url_base, day_path)
   html_url = "%s/%s/" % (html_url_base, day_path)
+  logging.info("md_url: %s" % md_url)
+  logging.info("html_url: %s" % html_url)
   md = "%s\n\n%s" % (html_url, urlopen(md_url).read().decode("utf-8"))
   bot.sendMessage(chat_id="-" + channel_id, text=md)
