dcc188b536641f63b31eb809b040d004e52bd611
vvasuki <vishvas.vasuki@gmail.com>
2020-12-16 15:25:30 +0530

    Fix bot

--- jyotisha/bot/telegram/__init__.py
+++ jyotisha/bot/telegram/__init__.py
@@ -14,7 +14,7 @@ def send_panchaanga(channel_id, token, computation_system_str, md_url_base, html
   today = bengaLUru.get_timezone_obj().current_time()
   out_path_md = get_canonical_path(city="", computation_system_str=computation_system_str, year=today.year, year_type=RulesRepo.ERA_GREGORIAN, output_dir=md_url_base)
   
-  day_path = "%s/%04d_monthly/%04d-%02d/%04d-%02d-%02d" % (out_path_md, today.year, today.month, today.year, today.month, today.day)
+  day_path = "%s/%04d_monthly/%04d-%02d/%04d-%02d-%02d" % (out_path_md, today.year, today.year, today.month, today.year, today.month, today.day)
   md_url = "%s/%s.md" % (md_url_base, day_path)
 
   out_path_html = get_canonical_path(city="", computation_system_str=computation_system_str, year=today.year, year_type=RulesRepo.ERA_GREGORIAN, output_dir=html_url_base)
