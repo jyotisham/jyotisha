d3333734ef47bd7be72e00a21970d0d444212a7f
vvasuki <vishvas.vasuki@gmail.com>
2020-12-23 14:55:57 +0530

    Add Chennai panchAnga.

--- jyotisha/panchaanga/writer/generation_project/__main__.py
+++ jyotisha/panchaanga/writer/generation_project/__main__.py
@@ -2,9 +2,14 @@ from jyotisha.panchaanga import spatio_temporal, temporal
 from jyotisha.panchaanga.temporal import era
 from jyotisha.panchaanga.writer import generation_project
 
+# bengaLUru
 bengaLUru = spatio_temporal.City.get_city_from_db("sahakAra nagar, bengaLUru")
 today = bengaLUru.get_timezone_obj().current_time()
 year = today.year
 # year = 2021
 generation_project.dump_detailed(year=year, city=bengaLUru, year_type=era.ERA_GREGORIAN, computation_system=temporal.get_kauNdinyAyana_bhAskara_gRhya_computation_system())
+generation_project.dump_detailed(year=year, city=bengaLUru, year_type=era.ERA_GREGORIAN)
+
+# chennai
+chennai = spatio_temporal.City.get_city_from_db("Chennai")
 generation_project.dump_detailed(year=year, city=bengaLUru, year_type=era.ERA_GREGORIAN)
