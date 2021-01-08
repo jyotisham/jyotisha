ed915aca5f50478784d5c1adee8b4d325cf7e271
vvasuki <vishvas.vasuki@gmail.com>
2021-01-08 13:07:23 +0530

    Better MD.

--- jyotisha/panchaanga/writer/md/day_details.py
+++ jyotisha/panchaanga/writer/md/day_details.py
@@ -167,15 +167,17 @@ def print_khachakra_stithi(daily_panchaanga, output_stream, script, subsection_m
   print('- २|🌛-🌞|%s  ' % (karana_data_str), file=output_stream)
   print('- 🌌🌛%s  ' % (chandrashtama_rashi_data_str), file=output_stream)
   if daily_panchaanga.mauDhyas is not None:
-    muuDha_grahas = ["%s (%.2f° → %.2f°)" % (translate_or_transliterate(text=names.NAMES["GRAHA_NAMES"]["sa"][g], script=script, source_script=sanscript.DEVANAGARI), angles[0], angles[1]) for g, angles in daily_panchaanga.mauDhyas.items()]
+    grahas = ["%s (%.2f° → %.2f°)" % (translate_or_transliterate(text=names.NAMES["GRAHA_NAMES"]["sa"][g], script=script, source_script=sanscript.DEVANAGARI), angles[0], angles[1]) for g, angles in daily_panchaanga.mauDhyas.items()]
     print("___________________", file=output_stream)
+    grahas = sorted(grahas)
     print(
-      '- 🌞-🪐 **%s** - %s' % (names.translate_or_transliterate(text="मूढग्रहाः", script=script), ", ".join(muuDha_grahas)),
+      '- 🌞-🪐 **%s** - %s' % (names.translate_or_transliterate(text="मूढग्रहाः", script=script), ", ".join(grahas)),
       file=output_stream)
   if daily_panchaanga.amauDhyas is not None:
     grahas = ["%s (%.2f° → %.2f°)" % (translate_or_transliterate(text=names.NAMES["GRAHA_NAMES"]["sa"][g], script=script, source_script=sanscript.DEVANAGARI), angles[0], angles[1]) for g, angles in daily_panchaanga.amauDhyas.items()]
+    grahas = sorted(grahas)
     print(
-      '- 🌞-🪐 **%s** - %s' % (names.translate_or_transliterate(text="मूढग्रहाः", script=script), ", ".join(grahas)),
+      '- 🌞-🪐 **%s** - %s' % (names.translate_or_transliterate(text="अमूढग्रहाः", script=script), ", ".join(grahas)),
       file=output_stream)
   print("___________________", file=output_stream)
 
