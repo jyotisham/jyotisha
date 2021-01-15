e7c6ccc7f743420a2150348f55bada593a7b2fef
Karthik Raman <karthikraman@users.noreply.github.com>
2021-01-15 20:07:37 +0530

    update tests/data

--- jyotisha_tests/temporal/festival/applier/solar_test.py
+++ jyotisha_tests/temporal/festival/applier/solar_test.py
@@ -10,15 +10,15 @@ def test_daily_solar_viSukkaNi():
   computation_system = ComputationSystem.DEFAULT
   panchaanga = periodical.Panchaanga(city=chennai, start_date=Date(2018, 4, 14), end_date=Date(2018, 4, 15), computation_system=computation_system)
  
-  assert "viSukkan2i" in panchaanga.date_str_to_panchaanga[Date(2018, 4, 14).get_date_str()].festival_id_to_instance
-  assert "viSukkan2i" not in panchaanga.date_str_to_panchaanga[Date(2018, 4, 15).get_date_str()].festival_id_to_instance.keys()
+  assert "viSukkan2i" not in panchaanga.date_str_to_panchaanga[Date(2018, 4, 14).get_date_str()].festival_id_to_instance
+  assert "viSukkan2i" in panchaanga.date_str_to_panchaanga[Date(2018, 4, 15).get_date_str()].festival_id_to_instance.keys()
 
 
 def test_periodic_solar_viSukkaNi():
   computation_system = ComputationSystem.DEFAULT
   panchaanga = periodical.Panchaanga(city=chennai, start_date=Date(2018, 4, 10), end_date=Date(2018, 4, 18), computation_system=computation_system)
-  assert "viSukkan2i" in panchaanga.date_str_to_panchaanga[Date(2018, 4, 14).get_date_str()].festival_id_to_instance
-  assert "viSukkan2i" not in panchaanga.date_str_to_panchaanga[Date(2018, 4, 15).get_date_str()].festival_id_to_instance
+  assert "viSukkan2i" not in panchaanga.date_str_to_panchaanga[Date(2018, 4, 14).get_date_str()].festival_id_to_instance
+  assert "viSukkan2i" in panchaanga.date_str_to_panchaanga[Date(2018, 4, 15).get_date_str()].festival_id_to_instance
 
 def test_periodic_solar_mUDavaN_muLukku():
   computation_system = ComputationSystem.DEFAULT
