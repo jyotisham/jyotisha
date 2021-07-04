e070b0398f7e5a7be289fd28d163d8025a2189d0
Karthik Raman <karthikraman@users.noreply.github.com>
2021-07-04 23:28:43 +0530

    Update update_test_data.sh

--- jyotisha_tests/update_test_data.sh
+++ jyotisha_tests/update_test_data.sh
@@ -2,5 +2,5 @@
 # This file is to be used after making some corrections to code, which will 
 # break tests. Running this file will update all the required files, so that future re-factoring etc.
 # can be done, with appropriate tests.
-find spatio_temporal/data/ -type f| grep -v timing | xargs rm
+find spatio_temporal/data/ -type f | grep -v timing | xargs rm
 pytest
