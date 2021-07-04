bb07145da0690d0cacd16756c6829ddd4dcf4dd2
Karthik Raman <karthikraman@users.noreply.github.com>
2021-07-04 10:10:43 +0530

    fixing error in in paraviddha dual assignments

--- jyotisha/panchaanga/temporal/festival/applier/__init__.py
+++ jyotisha/panchaanga/temporal/festival/applier/__init__.py
@@ -84,6 +84,12 @@ class FestivalAssigner(PeriodicPanchaangaApplier):
       # An error here implies the festival_id_to_instance were not assigned: adhika
       # mAsa calc errors??
 
+    # Remove paraviddha assigned on consecutive days
+    for d in range(self.panchaanga.duration_prior_padding, self.panchaanga.duration + self.panchaanga.duration_prior_padding):
+      for f in [fest.name for fest in self.daily_panchaangas[d].festival_id_to_instance.values()]:
+        if f in [fest.name for fest in self.daily_panchaangas[d + 1].festival_id_to_instance.values()]:
+          self.panchaanga.delete_festival_date(fest_id=f, date=self.daily_panchaangas[d].date)
+          logging.warning('%s on both days %d and %d! Deleted %d' % (f, d, d + 1, d))
 
 # Essential for depickling to work.
 common.update_json_class_index(sys.modules[__name__])
