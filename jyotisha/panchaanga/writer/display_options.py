ccf26d9e4c98e57475a122cb38b23eecda734d29
Karthik Raman <karthikraman@users.noreply.github.com>
2022-04-15 18:52:43 +0530

    lazy prioritisation/grouping of festivals - #124

--- jyotisha/panchaanga/writer/display_options.py
+++ jyotisha/panchaanga/writer/display_options.py
@@ -18,6 +18,29 @@ class DisplayOptions(JsonObject):
     return sorted(festival_id_to_instance.values())
 
 
+class KarthikDisplayOptions(JsonObject):
+  def __init__(self, scripts=None, languages=None):
+    self.scripts = scripts
+    self.languages = languages
+
+  def sort_festivals(self, festival_id_to_instance):
+    festivals_list = sorted(festival_id_to_instance.values())
+    prioritised_festivals_list = ['saGkramaNa', 'grahaNa', 'amAvAsyA', 'zrAddha',
+                                  'mAsaH', 'EkAdazI', 'vijayA/zravaNa', 'saMvatsaraH', 'harivAsaraH', 
+                                  'caturthI']
+    manually_prioritised_festivals = []
+
+    for f in list(festivals_list):
+      if any(match in f.name for match in prioritised_festivals_list):
+        festivals_list.remove(f)
+        manually_prioritised_festivals.append(f)
+
+    if manually_prioritised_festivals:
+      festivals_list = manually_prioritised_festivals + festivals_list
+
+    return festivals_list
+
+
 # Essential for depickling to work.
 common.update_json_class_index(sys.modules[__name__])
 # logging.debug(common.json_class_index)
