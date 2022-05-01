7ca65cd77fb2b8f9dc3ae4d4c7706eb72cf1eab9
Karthik Raman <karthikraman@users.noreply.github.com>
2022-05-01 07:32:02 +0530

    handling _or_ correctly

--- jyotisha/panchaanga/temporal/festival/__init__.py
+++ jyotisha/panchaanga/temporal/festival/__init__.py
@@ -130,7 +130,7 @@ class TransitionFestivalInstance(FestivalInstance):
 
 
 def get_description(festival_instance, fest_details_dict, script, truncate=True, header_md="#####"):
-  fest_id = festival_instance.name.replace('/', ' or ')
+  fest_id = festival_instance.name.replace('__', '_or_')
   desc = None
   if re.match('aGgArakI.*saGkaTahara-caturthI-vratam', fest_id):
     fest_id = fest_id.replace('aGgArakI~', '')
@@ -227,7 +227,7 @@ def get_description(festival_instance, fest_details_dict, script, truncate=True,
   return default_if_none(desc, "")
 
 def get_description_tex(festival_instance, fest_details_dict, script):
-  fest_id = festival_instance.name.replace('/', ' or ')
+  fest_id = festival_instance.name.replace('__', '_or_')
   desc = {}
   if re.match('aGgArakI.*saGkaTahara-caturthI-vratam', fest_id):
     fest_id = fest_id.replace('aGgArakI~', '')
