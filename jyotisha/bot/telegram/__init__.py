f66ba2d40b2eb447e739b5c4d99c2612f1c60d85
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-05-25 07:41:36 +0530

    xatra

--- jyotisha/bot/telegram/__init__.py
+++ jyotisha/bot/telegram/__init__.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime, timedelta
 from urllib.request import urlopen
 
 import regex
@@ -8,14 +9,19 @@ from jyotisha.panchaanga.temporal import era
 from jyotisha.panchaanga.writer.generation_project import get_canonical_path
 
 
-def send_panchaanga(city, channel_id, token, computation_system_str, md_url_base, html_url_base, dry_run=False):
+def send_panchaanga(city, channel_id, token, computation_system_str, md_url_base, html_url_base, date_str=None, next_day=False, dry_run=False):
   bot = telegram.Bot(token=token)
-  today = city.get_timezone_obj().current_time()
+  if date_str is None:
+    today = city.get_timezone_obj().current_time()
+    date_str = "%04d-%02d-%02d" % (today.year, today.month, today.day)
+  if next_day:
+    tomorrow = datetime.strptime(date_str, "%Y-%m-%d") + timedelta(days=1)
+    date_str = "%04d-%02d-%02d" % (tomorrow.year, tomorrow.month, tomorrow.day)
   import urllib
   city_url_segment = urllib.parse.quote(city.name)
   out_path_md = get_canonical_path(city=city_url_segment, computation_system_str=computation_system_str, year=today.year, year_type=era.ERA_GREGORIAN, output_dir=md_url_base)
 
-  md_url = "%s_monthly/%04d-%02d/%04d-%02d-%02d.md" % (out_path_md, today.year, today.month, today.year, today.month, today.day)
+  md_url = "%s_monthly/%s/%s.md" % (out_path_md, date_str[:7], date_str)
 
   city_url_segment = regex.sub("[^a-zA-Z]+", "-", city.name)
   out_path_html = get_canonical_path(city=city_url_segment, computation_system_str=computation_system_str, year=today.year, year_type=era.ERA_GREGORIAN, output_dir=html_url_base)
--- jyotisha/bot/telegram/__main__.py
+++ jyotisha/bot/telegram/__main__.py
@@ -4,7 +4,7 @@ from jyotisha.bot.telegram import send_panchaanga
 from jyotisha.panchaanga import spatio_temporal
 
 # Example arguments:
-# --token "????" --city "sahakAra nagar, bengaLUru" --channel_id 1001205695765 --md_url_base "https://raw.githubusercontent.com/jyotisham/jyotisha/generated-output/" --html_url_base "https://jyotisham.github.io/jyotisha/output/" --computation_system_str SOLSTICE_POST_DARK_10_ADHIKA__CHITRA_AT_180 --dry_run=False
+# --token "????" --city "sahakAra nagar, bengaLUru" --channel_id 1001205695765 --md_url_base "https://raw.githubusercontent.com/jyotisham/jyotisha/generated-output/" --html_url_base "https://jyotisham.github.io/jyotisha/output/" --computation_system_str SOLSTICE_POST_DARK_10_ADHIKA__CHITRA_AT_180 --next_day --dry_run
 
 parser = argparse.ArgumentParser(description='Telgram bot.')
 parser.add_argument('--city', type=str, default="sahakAra nagar, bengaLUru", nargs='?')
@@ -13,9 +13,11 @@ parser.add_argument('--token', type=str, nargs='?')
 parser.add_argument('--computation_system_str', type=str, nargs='?')
 parser.add_argument('--md_url_base', type=str, nargs='?')
 parser.add_argument('--html_url_base', type=str, nargs='?')
-parser.add_argument('--dry_run', type=bool, default=False, nargs='?') # Just use --dry_run, and not --dry_run=False.
+parser.add_argument('--date', type=str, default=None, nargs='?')
+parser.add_argument('--next_day', action=argparse.BooleanOptionalAction, default=False)
+parser.add_argument('--dry_run', action=argparse.BooleanOptionalAction, default=False)
 args = parser.parse_args()
 
 city = spatio_temporal.City.get_city_from_db(args.city)
 
-send_panchaanga(city=city, channel_id=args.channel_id, token=args.token, md_url_base=args.md_url_base, computation_system_str=args.computation_system_str, html_url_base=args.html_url_base, dry_run=args.dry_run)
+send_panchaanga(city=city, channel_id=args.channel_id, token=args.token, md_url_base=args.md_url_base, computation_system_str=args.computation_system_str, html_url_base=args.html_url_base, date_str=args.date, next_day=args.next_day, dry_run=args.dry_run)
