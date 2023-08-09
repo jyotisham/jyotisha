c8b95e42cfb2248648428d06ae23709f8a4511ef
Karthik Raman <karthikraman@users.noreply.github.com>
2023-08-09 11:00:39 +0530

    priority error check

--- jyotisha/panchaanga/temporal/festival/priority_decision.py
+++ jyotisha/panchaanga/temporal/festival/priority_decision.py
@@ -172,5 +172,7 @@ def decide(p0, p1, target_anga, kaala, priority, ayanaamsha_id):
     decision = decide_puurvaviddha(p0=p0, p1=p1, target_anga=target_anga, kaala=kaala)
   elif priority == 'vyaapti':
     decision = decide_vyaapti(p0=p0, p1=p1, target_anga=target_anga, kaala=kaala, ayanaamsha_id=ayanaamsha_id)
+  else:
+    ValueError('Unknown priority %s' % priority)
   return decision
 
