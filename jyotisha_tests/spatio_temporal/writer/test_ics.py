1657bce18ac6d04b4ed8cd1ac6cd8379a1188750
Karthik Raman <karthikraman@users.noreply.github.com>
2024-04-13 10:57:45 +0530

    ignore DTSTAMP in ics comparison

--- jyotisha_tests/spatio_temporal/writer/test_ics.py
+++ jyotisha_tests/spatio_temporal/writer/test_ics.py
@@ -1,25 +1,14 @@
 import logging
 import os
 
-# from jyotisha.panchaanga.spatio_temporal import City, annual
-# from jyotisha.panchaanga.writer.write_daily_panchaanga_tex import writeDailyTeX
 from indic_transliteration import sanscript
-
 from jyotisha.panchaanga.writer.ics import compute_calendar, write_to_file
 from jyotisha.panchaanga.spatio_temporal.periodical import Panchaanga
-
-# import swisseph as swe
-# from indic_transliteration import sanscript
-
-# from jyotisha.panchaanga import scripts
-# from jyotisha.panchaanga.spatio_temporal import annual
 
 logging.basicConfig(
   level=logging.DEBUG,
   format="%(levelname)s: %(asctime)s {%(filename)s:%(lineno)d}: %(message)s "
 )
-
-
 
 TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
 
@@ -33,12 +22,18 @@ def test_panchanga_chennai_2019():
   if not os.path.exists(orig_ics_file):
     logging.warning("%s not present. Assuming that it was deliberately deleted to update test files.", orig_ics_file)
     write_to_file(ics_calendar, orig_ics_file)
-    
 
-  with open(orig_ics_file) as orig_tex:
-    with open(current_ics_output) as current_tex:
-      assert current_tex.read() == orig_tex.read()
-
+  ics_file_match = True
+  with open(orig_ics_file) as orig_ics:
+    with open(current_ics_output) as current_ics:
+      for line1, line2 in zip(current_ics, orig_ics):
+        if line1.startswith("DTSTAMP") and line2.startswith("DTSTAMP"):
+          continue
+        if line1 != line2:
+          ics_file_match = False
+          break
+  
+  assert ics_file_match == True
 
 if __name__ == '__main__':
   # test_panchanga_chennai_2018()
