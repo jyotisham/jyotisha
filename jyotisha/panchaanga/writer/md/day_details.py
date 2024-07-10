0f9eb763d08635dc801fadb81410a2c31772a1af
vvasuki <vishvas.vasuki@gmail.com>
2024-07-10 14:16:33 +0530

    Better tropical season computation

--- jyotisha/panchaanga/writer/md/day_details.py
+++ jyotisha/panchaanga/writer/md/day_details.py
@@ -107,7 +107,7 @@ def print_ayana_Rtu_maasa_info(daily_panchaanga, output_stream, script):
   ayanam_sidereal = names.NAMES['AYANA_NAMES']['sa'][script][daily_panchaanga.solar_sidereal_date_sunset.month]
   ayanam = names.NAMES['AYANA_NAMES']['sa'][script][daily_panchaanga.tropical_date_sunset.month]
   rtu_solar = names.NAMES['RTU_NAMES']['sa'][script][daily_panchaanga.solar_sidereal_date_sunset.month]
-  rtu_tropical = names.NAMES['RTU_NAMES']['sa'][script][daily_panchaanga.tropical_date_sunset.month]
+  rtu_tropical = names.NAMES['RTU_NAMES']['sa'][script][daily_panchaanga.tropical_date_sunset.month % 12 + 1]
   rtu_lunar = names.NAMES['RTU_NAMES']['sa'][script][int(ceil(daily_panchaanga.lunar_date.month.index))]
 
   print("___________________", file=output_stream)
