2e1e240255d483575d63f7af2ac3da46038f597d
Karthik Raman <karthikraman@users.noreply.github.com>
2024-08-18 20:21:46 +0530

    do cleanup last

--- jyotisha/panchaanga/spatio_temporal/periodical.py
+++ jyotisha/panchaanga/spatio_temporal/periodical.py
@@ -164,11 +164,11 @@ class Panchaanga(common.JsonObject):
     solar.SolarFestivalAssigner(panchaanga=self).assign_all()
     vaara.VaraFestivalAssigner(panchaanga=self).assign_all()
     generic_assigner = FestivalAssigner(panchaanga=self)
-    generic_assigner.cleanup_festivals()
     rule_lookup_assigner.assign_relative_festivals()
     # self._sync_festivals_dict_and_daily_festivals(here_to_daily=True, daily_to_here=True)
     generic_assigner.assign_festival_numbers()
     tithi_festival.TithiFestivalAssigner(panchaanga=self).assign_relative_anadhyayana_days()
+    generic_assigner.cleanup_festivals()
     self.clear_padding_day_festivals()
 
 
