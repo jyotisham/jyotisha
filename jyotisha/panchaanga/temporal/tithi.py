fd48139eea5031baa74d4f12a176e356b9622044
Karthik Raman <karthikraman@users.noreply.github.com>
2025-04-13 09:25:03 +0530

    correcting logic

--- jyotisha/panchaanga/temporal/tithi.py
+++ jyotisha/panchaanga/temporal/tithi.py
@@ -6,6 +6,7 @@ from jyotisha.panchaanga.temporal import PeriodicPanchaangaApplier, interval, ge
 from jyotisha.panchaanga.temporal import time
 from jyotisha.panchaanga.temporal.zodiac.angas import AngaType, Anga
 from jyotisha.panchaanga.temporal.festival import FestivalInstance
+from jyotisha.panchaanga.temporal.festival.applier import solar
 
 from sanskrit_data.schema import common
 
@@ -140,15 +141,18 @@ class ShraadhaTithiAssigner(PeriodicPanchaangaApplier):
                                             interval=self.panchaanga.daily_panchaanga_for_date(nap_end_dt).get_interval(interval_id="full_day")), date=nap_end_dt)
 
     # Compute Sankranti Days
-    sankranti_dushta_days = []
-    for dp in self.panchaanga.daily_panchaangas_sorted()[2:self.panchaanga.duration + 2]:
-      if dp.solar_sidereal_date_sunset.month_transition is not None:
-        madhyaraatri_start = dp.day_length_based_periods.fifteen_fold_division.vaidhaatra.jd_start
-        madhyaraatri_end = dp.day_length_based_periods.fifteen_fold_division.vaidhaatra.jd_end
-        if dp.solar_sidereal_date_sunset.month_transition < madhyaraatri_start:
-          sankranti_dushta_days.append(dp.date)
-        else:
-          sankranti_dushta_days.append(dp.date + 1)
+    # sankranti_dushta_days = []
+    sankranti_dushta_days = solar.SolarFestivalAssigner(panchaanga=self.panchaanga).assign_sidereal_sankranti_punyakaala(force_computation=True)
+
+    # for dp in self.panchaanga.daily_panchaangas_sorted()[2:self.panchaanga.duration + 2]:
+    #   if dp.solar_sidereal_date_sunset.month_transition is not None:
+    #     madhyaraatri_start = dp.day_length_based_periods.fifteen_fold_division.vaidhaatra.jd_start
+    #     madhyaraatri_end = dp.day_length_based_periods.fifteen_fold_division.vaidhaatra.jd_end
+    #     #TODO: Should ideally assign based on the punyakaala day!
+    #     if dp.solar_sidereal_date_sunset.month_transition < madhyaraatri_start:
+    #       sankranti_dushta_days.append(dp.date)
+    #     else:
+    #       sankranti_dushta_days.append(dp.date + 1)
 
     # Compute Solar Month Tithis
     solar_tithi_days = [{t: [] for t in range(0, 32)} for _m in range(13)]
