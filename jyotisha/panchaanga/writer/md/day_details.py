9f022baea4eb49429dca3b40a459738a0fbdc7af
vvasuki <vishvas.vasuki@gmail.com>
2025-06-07 08:08:12 +0530

    Fix tests.

--- jyotisha/panchaanga/writer/md/day_details.py
+++ jyotisha/panchaanga/writer/md/day_details.py
@@ -182,7 +182,7 @@ def print_khachakra_stithi(daily_panchaanga, output_stream, script, subsection_m
   karana_data_str = daily_panchaanga.sunrise_day_angas.get_anga_data_md(anga_type=AngaType.KARANA, script=script,
                                                                         reference_jd=daily_panchaanga.julian_day_start)
   print('- २|🌛-🌞|%s  ' % (karana_data_str), file=output_stream)
-  print('- 🌌🌛%s  ' % (chandrashtama_rashi_data_str), file=output_stream)
+  # print('- 🌌🌛%s  ' % (chandrashtama_rashi_data_str), file=output_stream)
   if daily_panchaanga.mauDhyas is not None:
     grahas = ["%s (%.2f° → %.2f°)" % (translate_or_transliterate(text=names.NAMES["GRAHA_NAMES"]["sa"][g], script=script, source_script=sanscript.DEVANAGARI), angles[0], angles[1]) for g, angles in daily_panchaanga.mauDhyas.items()]
     print("___________________", file=output_stream)
